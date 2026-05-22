85700375b76a07a80e214dad7b4dcc278be8772b
Regis Freyd <djaiss@users.noreply.github.com>
2026-05-21 21:42:00 -0400

    wip

--- app/Http/Controllers/Marketing/Docs/ApiIntroductionController.php
+++ app/Http/Controllers/Marketing/Docs/ApiIntroductionController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Marketing\Docs;
 
 use App\Http\Controllers\Controller;
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View;
 
 class ApiIntroductionController extends Controller
 {
